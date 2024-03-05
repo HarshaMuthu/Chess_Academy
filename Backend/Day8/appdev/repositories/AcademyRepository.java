22dc0f3ae53e4f6c648e172d09b9acd858606b27
HarshaMuthu <113664562+HarshaMuthu@users.noreply.github.com>
2024-03-05 11:44:45 +0530

    Delete Backend/Day8 directory

--- Backend/Day8/appdev/repositories/AcademyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.chessacad.appdev.repositories;
-
-import com.chessacad.appdev.models.Academy;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AcademyRepository extends JpaRepository<Academy, Integer> {
-}
--- Backend/Day8/appdev/repositories/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.chessacad.appdev.repositories;
-
-import com.chessacad.appdev.models.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Integer> {
-}
