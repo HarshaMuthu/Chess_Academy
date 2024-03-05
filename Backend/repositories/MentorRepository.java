8e90bce34e5d230f705a90302ba94d065d17c4a0
HarshaMuthu <113664562+HarshaMuthu@users.noreply.github.com>
2024-03-05 11:40:55 +0530

    Delete Backend/repositories directory

--- Backend/repositories/MentorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.chessacad.appdev.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.chessacad.appdev.models.Mentor;
-
-public interface MentorRepository extends JpaRepository<Mentor, Integer> {
-}
--- Backend/repositories/UserApplicationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.chessacad.appdev.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.chessacad.appdev.models.UserApplication;
-
-public interface UserApplicationRepository extends JpaRepository<UserApplication, Integer> {
-}
